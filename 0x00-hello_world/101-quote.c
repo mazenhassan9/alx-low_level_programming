6e91473f8dd2ca4c9f66e992c1a9cd4cfb7906c6
mazenhassan9 <mazen.a.hassan99@gmail.com>
2023-06-14 23:21:51 -0700

    so

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,7 @@
 */
 int main(void)
 {
-const char *message = "and that piece of art is useful\" - Dora Korpar, "
-"2015-10-19\n";
+char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 ssize_t len = strlen(message);
 syscall(SYS_write, STDERR_FILENO, message, len);
 return (1);
