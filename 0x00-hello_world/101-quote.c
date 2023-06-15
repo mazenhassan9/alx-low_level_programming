3d5d955fd14ddc2b4e5bb585772f969222891187
mazenhassan9 <mazen.a.hassan99@gmail.com>
2023-06-14 23:10:23 -0700

    Bouns tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <string.h>
+#include <sys/syscall.h>
+/**
+* main - Entry point
+*
+* Return: Always 1
+*/
+int main(void)
+{
+const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+ssize_t len = strlen(message);
+syscall(SYS_write, STDERR_FILENO, message, len); 
+return (1);
+}
