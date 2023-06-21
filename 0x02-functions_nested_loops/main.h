f4f802f287e282bbc69198cf962bbb867b1d3bcf
mazenhassan9 <mazen.a.hassan99@gmail.com>
2023-06-21 11:52:27 -0700

    Modify main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,19 +1,16 @@
-#include "_putchar.h"
-/**
-* main - Entry point of the program
-*
-* Return: Always 0 (Success)
-*/
-int main(void)
-{
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
-}
+#ifndef main_h
+#define main_h
+extern int _putchar(char c);
+extern void print_alphabet(void);
+extern void print_alphabet_x10(void);
+extern int _islower(int c);
+extern int _isalpha(int c);
+extern int print_sign(int n);
+extern int _abs(int);
+extern int print_last_digit(int);
+extern void jack_bauer(void);
+extern void times_table(void);
+extern int add(int, int);
+extern void print_to_98(int n);
+extern void print_times_table(int n);
+#endif
